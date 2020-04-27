0f2006743c947fa4ee31b12f8dc752d8a6572dbf
Ulrika Sniedze <u.sniedze@gmail.com>
2020-04-27 10:37:25 +0200

    auto-commit

---
+++ myapp/precache-manifest.10ad778f9bc19032908740c9d1d671ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec309f36dab6ef67b7d7c5de4fc0b494",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "369007b83150a951291f",
+    "url": "/myapp/static/css/2.e50de653.chunk.css"
+  },
+  {
+    "revision": "a60565b1ae199a20b6fa",
+    "url": "/myapp/static/css/main.6091a8b1.chunk.css"
+  },
+  {
+    "revision": "369007b83150a951291f",
+    "url": "/myapp/static/js/2.9c2640fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myapp/static/js/2.9c2640fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a60565b1ae199a20b6fa",
+    "url": "/myapp/static/js/main.5198b3b7.chunk.js"
+  },
+  {
+    "revision": "4d63393204ca2c7465a6",
+    "url": "/myapp/static/js/runtime-main.7ebc8dbb.js"
+  },
+  {
+    "revision": "46bba648cf4e065061c86f9b57f43512",
+    "url": "/myapp/static/media/biohazard.46bba648.png"
+  }
+]);
